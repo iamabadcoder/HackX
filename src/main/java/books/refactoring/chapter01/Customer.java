cc2c048e4fc386e052d648d947308fc931ef3c45
caolei.cl <caolei.cl@alibaba-inc.com>
2016-08-01 08:50:45 +0800

    重构chapter01-part

---
+++ src/main/java/books/refactoring/chapter01/Customer.java
@@ -0,0 +1,67 @@
+package books.refactoring.chapter01;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ * Created by hackx on 7/31/16.
+ */
+public class Customer {
+
+    private String name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addRentals(Rental rental) {
+        _rentals.addElement(rental);
+    }
+
+    public String statement() {
+
+        Enumeration rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+        }
+
+        // add footer lines
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints(each.getDayRented());
+        }
+        return result;
+    }
+
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
+        return result;
+    }
+}
---
+++ src/main/java/books/refactoring/chapter01/Rental.java
@@ -0,0 +1,39 @@
+package books.refactoring.chapter01;
+
+/**
+ * Created by hackx on 7/31/16.
+ */
+public class Rental {
+
+    private Movie movie;
+    private int dayRented;
+
+    public Rental(Movie movie, int dayRented) {
+        this.movie = movie;
+        this.dayRented = dayRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public int getDayRented() {
+        return dayRented;
+    }
+
+    public void setDayRented(int dayRented) {
+        this.dayRented = dayRented;
+    }
+
+    double getCharge() {
+        return movie.getCharge(dayRented);
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return movie.getFrequentRenterPoints(daysRented);
+    }
+}
