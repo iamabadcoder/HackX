3a1e891969537cba5d8a366d85cda27ba8891764
caolei.cl <caolei.cl@alibaba-inc.com>
2016-08-21 10:03:17 +0800

    添加spark  demo

--- src/main/java/com/hackx/spark/HelloSpark.java
+++ src/main/java/com/hackx/spark/HelloSpark.java
@@ -18,13 +18,23 @@ public class HelloSpark {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("HelloSpark Application");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> rdd = sc.parallelize(data, 1);
+
+        List<Integer> lines = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> rdd = sc.parallelize(lines, 1);
         JavaRDD<Integer> result = rdd.map(new Function<Integer, Integer>() {
             public Integer call(Integer integer) throws Exception {
                 return integer * integer;
             }
         });
         System.out.println(StringUtils.join(result.collect(), ","));
+
+//        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+//        JavaRDD<Integer> rdd = sc.parallelize(data, 1);
+//        JavaRDD<Integer> result = rdd.map(new Function<Integer, Integer>() {
+//            public Integer call(Integer integer) throws Exception {
+//                return integer * integer;
+//            }
+//        });
+//        System.out.println(StringUtils.join(result.collect(), ","));
     }
 }
